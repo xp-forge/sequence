71c8958e422dcb041fac1c7a1793511c56e878bb
Timm Friebe <thekid@thekid.de>
2014-08-16 19:53:53 +0200

    Refactor: Pass function type to check for to Closure::of()

--- src/test/php/util/data/unittest/SequenceCreationTest.class.php
+++ src/test/php/util/data/unittest/SequenceCreationTest.class.php
@@ -20,9 +20,14 @@ class SequenceCreationTest extends AbstractSequenceTest {
     Sequence::of($input);
   }
 
-  #[@test, @values('callables')]
-  public function can_create_via_iterate($input, $name) {
-    $this->assertInstanceOf('util.data.Sequence', Sequence::iterate(0, $input), $name);
+  #[@test]
+  public function can_create_via_iterate() {
+    $this->assertInstanceOf('util.data.Sequence', Sequence::iterate(0, function($i) { return ++$i; }));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function cannot_create_via_iterate_when_arg_is_missing() {
+    $this->assertInstanceOf('util.data.Sequence', Sequence::iterate(0, function() { /* Empty */ }));
   }
 
   #[@test, @expect('lang.IllegalArgumentException'), @values('noncallables')]
@@ -30,9 +35,14 @@ class SequenceCreationTest extends AbstractSequenceTest {
     Sequence::iterate(0, $input);
   }
 
-  #[@test, @values('callables')]
-  public function can_create_via_generate($input) {
-    $this->assertInstanceOf('util.data.Sequence', Sequence::generate($input));
+  #[@test]
+  public function can_create_via_generate() {
+    $this->assertInstanceOf('util.data.Sequence', Sequence::generate(function() { return rand(0, 1000); }));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function cannot_create_via_iterate_when_excess_arg_present() {
+    $this->assertInstanceOf('util.data.Sequence', Sequence::generate(function($i) { /* Empty */ }));
   }
 
   #[@test, @expect('lang.IllegalArgumentException'), @values('noncallables')]

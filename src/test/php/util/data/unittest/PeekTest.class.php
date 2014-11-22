a757c0853fdf678dbae90dfdaf4e69b052f39d3e
Timm Friebe <thekid@thekid.de>
2014-11-22 16:18:38 +0100

    Add support for keys to peek() method

---
+++ src/test/php/util/data/unittest/PeekTest.class.php
@@ -0,0 +1,42 @@
+<?php namespace util\data\unittest;
+
+use util\data\Sequence;
+use io\streams\MemoryOutputStream;
+use util\cmd\Console;
+
+class PeekTest extends AbstractSequenceTest {
+
+  #[@test]
+  public function values() {
+    $debug= [];
+    Sequence::of([1, 2, 3, 4])
+      ->filter(function($e) { return $e % 2 > 0; })
+      ->peek(function($e) use(&$debug) { $debug[]= $e; })
+      ->toArray()   // or any other terminal action
+    ;
+    $this->assertEquals([1, 3], $debug);
+  }
+
+  #[@test]
+  public function keys() {
+    $debug= [];
+    Sequence::of([1, 2, 3, 4])
+      ->filter(function($e) { return $e % 2 > 0; })
+      ->peek(function($e, $key) use(&$debug) { $debug[]= $key; })
+      ->toArray()   // or any other terminal action
+    ;
+    $this->assertEquals([0, 2], $debug);
+  }
+
+  #[@test]
+  public function writing_to_console_out() {
+    $orig= Console::$out->getStream();
+    $out= new MemoryOutputStream();
+    Console::$out->setStream($out);
+
+    Sequence::of([1, 2, 3, 4])->peek('util.cmd.Console::write')->toArray();
+
+    Console::$out->setStream($orig);    
+    $this->assertEquals('1234', $out->getBytes());
+  }
+}

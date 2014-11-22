1ebddc2895a5af57bd72ed0b07cf83ea6ed729f7
Timm Friebe <thekid@thekid.de>
2014-11-22 14:16:32 +0100

    Move collection tests to their own test class

---
+++ src/test/php/util/data/unittest/SequenceCollectionTest.class.php
@@ -0,0 +1,25 @@
+<?php namespace util\data\unittest;
+
+use util\data\Sequence;
+
+class SequenceCollectionTest extends AbstractSequenceTest {
+
+  #[@test]
+  public function used_for_averaging() {
+    $result= Sequence::of([1, 2, 3, 4])->collect(new Collector(
+      function() { return ['total' => 0, 'sum' => 0]; },
+      function(&$result, $arg) { $result['total']++; $result['sum']+= $arg; }
+    ));
+    $this->assertEquals(2.5, $result['sum'] / $result['total']);
+  }
+
+  #[@test]
+  public function used_for_joining() {
+    $result= Sequence::of(['a', 'b', 'c'])->collect(new Collector(
+      function() { return ''; },
+      function(&$result, $arg) { $result.= ', '.$arg; },
+      function($result) { return substr($result, 2); }
+    ));
+    $this->assertEquals('a, b, c', $result);
+  }
+}

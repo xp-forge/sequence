e59825372e95fc9c6bb46f0ed61101d0681990f9
Timm Friebe <thekid@thekid.de>
2017-03-18 13:44:57 +0100

    Add tests for yielding

--- src/main/php/util/data/YieldingOf.class.php
+++ src/main/php/util/data/YieldingOf.class.php
@@ -3,6 +3,8 @@
 /**
  * Special case of traversal wrapper geared towards generators, where
  * rewind() does not fail until the generator was completely yielded.
+ *
+ * @test  xp://util.data.unittest.YieldingOfTest
  */
 class YieldingOf extends Iterator {
   private $started= false;
@@ -18,13 +20,7 @@ class YieldingOf extends Iterator {
       throw new CannotReset('Yielding from generator previously started, cannot rewind');
     }
 
-    try {
-      $this->it->rewind();
-      $this->started= true;
-    } catch (\Throwable $e) {   // PHP7
-      throw new CannotReset($e->getMessage(), $e);
-    } catch (\Exception $e) {   // PHP5
-      throw new CannotReset($e->getMessage(), $e);
-    }
+    $this->it->rewind();
+    $this->started= true;
   }
 }
